8d1454a2c0dc4bf77db20a7b53de67521dd7e081
mybluewebsite <mybluewebsite@dontsp.am>
2025-07-08 19:28:44 +0100

    removed the secret key from settings.py

--- env.py
+++ env.py
@@ -2,3 +2,4 @@ import os
 
 os.environ.setdefault(
     "DATABASE_URL", "postgresql://neondb_owner:npg_NvW4D6aHjUme@ep-quiet-grass-a2t7do3v.eu-central-1.aws.neon.tech/acorn_moist_rival_78870")
+os.environ.setdefault("SECRET_KEY", "django-insecure-ivu!(fl!pe(0!f(32)+g06-f-s(7v(2e$r8#iooef*g&0r@oqw")
