6a1bcb9f6261a2debb745d22ad8001b70be78f23
mybluewebsite <mybluewebsite@dontsp.am>
2025-07-09 21:20:11 +0100

    add a site register for admin and migrate changes

--- stories/admin.py
+++ stories/admin.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from .models import Tale, Suggestion
 from django_summernote.admin import SummernoteModelAdmin
 
+@admin.register(Tale)
 class TaleAdmin(SummernoteModelAdmin):
 
     list_display = ('title', 'slug', 'status')
@@ -10,5 +11,5 @@ class TaleAdmin(SummernoteModelAdmin):
     prepopulated_fields = {'slug': ('title',)}
     summernote_fields = ('content',)
 
-admin.site.register(Tale)
+
 admin.site.register(Suggestion)
