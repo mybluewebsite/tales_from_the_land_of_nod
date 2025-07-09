c609afed4d6c3676b184f41793fc77b83ff276fb
mybluewebsite <mybluewebsite@dontsp.am>
2025-07-09 21:06:43 +0100

    amend urls.py and views.py

--- stories/urls.py
+++ stories/urls.py
@@ -2,5 +2,5 @@ from . import views
 from django.urls import path
 
 urlpatterns = [
-    path("", views.MyStoriesView.as_view(), name="my_stories"),
+    path("", views.MyStories.as_view(), name="stories"),
 ]
--- stories/views.py
+++ stories/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.views import generic
 from .models import Tale
 
-class MyStoriesView(generic.ListView):
-    queryset = Tale.objects.all()
+class MyStories(generic.ListView):
+    queryset = Tale.objects.filter(status=1).order_by("-created_on")
+    context_object_name = "stories"
     template_name = "my_stories.html"
