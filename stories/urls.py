5f0c8e7aeedff1ee2f96568b26d9cfd406ec46f1
mybluewebsite <mybluewebsite@dontsp.am>
2025-07-12 19:37:14 +0100

    add images file, add tale_detail.html and reference requirements in views.py, and urls.py

--- stories/urls.py
+++ stories/urls.py
@@ -3,4 +3,5 @@ from django.urls import path
 
 urlpatterns = [
     path("", views.TaleList.as_view(), name="stories"),
+    path('<slug:slug>/', views.tale_detail, name='tale_detail'),
 ]
--- stories/views.py
+++ stories/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Tale
 
@@ -7,3 +7,26 @@ class TaleList(generic.ListView):
     context_object_name = "stories"
     template_name = "stories/index.html"
     paginate_by = 3
+
+def tale_detail(request, slug):
+    """
+    Display an individual :model:`stories.Tale`.
+
+    **Context**
+
+    ``post``
+        An instance of :model:`stories.Tale`.
+
+    **Template:**
+
+    :template:`stories/tale_detail.html`
+    """
+
+    queryset = Tale.objects.filter(status=1)
+    post = get_object_or_404(queryset, slug=slug)
+
+    return render(
+        request,
+        "stories/tale_detail.html",
+        {"tale": post},
+    )
