2e5d1804fbf57fa961efa54e6adb3010325527b3
mybluewebsite <mybluewebsite@dontsp.am>
2025-07-22 19:10:53 +0100

    added custom code for showcase .py files

--- showcase/admin.py
+++ showcase/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from .models import Showcase
+from .models import Showcase, CollaborateRequest
 from django_summernote.admin import SummernoteModelAdmin
 
 
@@ -7,3 +7,8 @@ from django_summernote.admin import SummernoteModelAdmin
 class ShowcaseAdmin(SummernoteModelAdmin):
 
     summernote_fields = ('content',)
+
+@admin.register(CollaborateRequest)
+class CollaborateRequestAdmin(admin.ModelAdmin):
+
+    list_display = ('message', 'read',)
