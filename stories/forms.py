0fe68fc2f3cf73fb185801bf711ecd15b5fb6af0
mybluewebsite <mybluewebsite@dontsp.am>
2025-07-25 22:41:02 +0100

    amend requirements.txt file

--- stories/forms.py
+++ stories/forms.py
@@ -6,3 +6,9 @@ class SuggestionForm(forms.ModelForm):
     class Meta:
         model = Suggestion
         fields = ('body',)
+        widgets = {
+            'body': forms.Textarea(attrs={
+                'rows': 4,
+                'placeholder': 'Enter your suggestion here...'
+            })
+        }
--- stories/models.py
+++ stories/models.py
@@ -6,9 +6,7 @@ STATUS = ((0, "Draft"), (1, "Published"))
 class Tale(models.Model):
     title = models.CharField(max_length=200, unique=True)
     slug = models.SlugField(max_length=200, unique=True)
-    author = models.ForeignKey(
-    User, on_delete=models.CASCADE, related_name="bedtime_stories"
-)
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bedtime_stories")
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     blurb = models.TextField(blank=True)
@@ -22,10 +20,8 @@ class Tale(models.Model):
         return f"{self.title} | written by {self.author}"
 
 class Suggestion(models.Model):
-    tale = models.ForeignKey(
-        Tale, on_delete=models.CASCADE, related_name="suggestions")
-    author = models.ForeignKey(
-        User, on_delete=models.CASCADE, related_name="commenter")
+    tale = models.ForeignKey(Tale, on_delete=models.CASCADE, related_name="suggestions")
+    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="suggestor")
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
