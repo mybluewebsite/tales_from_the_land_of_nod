6df39c44f811bac80318904b95121f498a074098
mybluewebsite <mybluewebsite@dontsp.am>
2025-07-26 20:17:00 +0100

    amend stykling of the index.html and showcase.html,  remopved categories model

--- stories/models.py
+++ stories/models.py
@@ -8,7 +8,6 @@ class Tale(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(User, on_delete=models.CASCADE, related_name="bedtime_stories")
     content = models.TextField()
-    category = models.CharField(max_length=100, blank=True)
     created_on = models.DateTimeField(auto_now_add=True)
     blurb = models.TextField(blank=True)
     status = models.IntegerField(choices=STATUS, default=0)
